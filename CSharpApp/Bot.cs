f80c5268d4920e676b2a4173ff216767e7eab285
Josh Eldridge <Joshua.Eldridge@blujaysolutions.com>
2018-11-28 09:06:50 -0500

    Made the bot safer

--- CSharpApp/Bot.cs
+++ CSharpApp/Bot.cs
@@ -7,9 +7,25 @@ namespace SeeSharpBotty
 {
     public class Bot
     {
-        public static void Main(string[] args)
-            => new Bot().MainAsync().GetAwaiter().GetResult();
+        private string Token { get; set; }
 
+        public static void Main(string[] args)
+        {
+            if (args.Length == 1)
+            {
+                new Bot(args[0]).MainAsync().GetAwaiter().GetResult();
+            }
+            else
+            {
+                Console.WriteLine("Please enter the bot's token.");
+                Console.ReadKey();
+            }
+        }
+
+        public Bot(string token)
+        {
+            Token = token;
+        }
 
         private async Task MainAsync()
         {
@@ -18,27 +34,28 @@ namespace SeeSharpBotty
             client.Log += Log;
             client.MessageReceived += MessageReceived;
 
-            const string token = "NTE2NzIxMTMwMDgyNDAyMzE5.Dt3yzQ.379PqQLMxzYkaOGtLa8ETIKwbFI"; // Remember to keep this private!
-            await client.LoginAsync(TokenType.Bot, token);
+            await client.LoginAsync(TokenType.Bot, Token);
             await client.StartAsync();
 
             // Block this task until the program is closed.
             await Task.Delay(-1);
         }
 
-        private Task Log(LogMessage msg)
+        private static Task Log(LogMessage msg)
         {
             Console.WriteLine(msg.ToString());
             return Task.CompletedTask;
         }
 
-        private async Task MessageReceived(SocketMessage message)
+        private static async Task MessageReceived(SocketMessage message)
         {
-            Console.WriteLine($"\n\nAuthor: {message.Author.Username}\nChannel: {message.Channel.Name}\nMessage: {message.Content}");
+            Console.WriteLine(
+                $"\n\nAuthor: {message.Author.Username}\nChannel: {message.Channel.Name}\nMessage: {message.Content}");
             if (message.Content.ToUpper().Contains("SEE"))
             {
                 await message.Author.SendMessageAsync("I see you.");
             }
+
             if (message.Content == "!ping")
             {
                 await message.Channel.SendMessageAsync("Pong!");
